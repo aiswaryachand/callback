9b84a3729e4a7a97a87b74aef4c8dc4720f23933
Aiswarya_Purushothaman <133517332+aiswaryachand@users.noreply.github.com>
2023-10-01 16:49:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var span = document.getElementById("span");
+span.innerHTML =10
+var count = 10
+setTimeout(()=>{
+    count =count-1
+    span.innerHTML = count
+setTimeout(()=>{
+        count =count-1
+        span.innerHTML = count
+setTimeout(()=>{
+            count =count-1
+            span.innerHTML = count
+setTimeout(()=>{
+           count =count-1
+           span.innerHTML = count
+setTimeout(()=>{
+         count =count-1
+         span.innerHTML = count
+setTimeout(()=>{
+         count =count-1
+         span.innerHTML = count
+setTimeout(()=>{
+    count =count-1
+   span.innerHTML = count
+setTimeout(()=>{
+   count =count-1
+  span.innerHTML = count
+  setTimeout(()=>{
+    count =count-1
+   span.innerHTML = count
+setTimeout(()=>{
+  count =count-1
+  span.innerHTML = "Happy Independence Day"                                   
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
